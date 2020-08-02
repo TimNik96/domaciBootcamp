9e81aa1f1a6a3d16779789bededc56bcd32b1952
tim <timotic.nikola.27@gmail.com>
2020-08-02 12:27:11 +0200

    ispravka prvog zadatka za domaci u sredu

--- Nedelja_3/Sreda/zadatak1.js
+++ Nedelja_3/Sreda/zadatak1.js
@@ -1,15 +1,17 @@
-function maximum(n, j, k){
+function maximum(n, j, k) {
     let max = n;
-    if(j > max){
+    if (j > max) {
         max = j;
-    }else if(k > max){
+    } 
+    if (k > max) {
         max = k;
     }
     return max;
+
 }
 
-let broj1 = 15;
-let broj2 = 35;
-let broj3 = 25;
+let broj1 = 130;
+let broj2 = 250;
+let broj3 = 105;
 
 console.log(maximum(broj1, broj2, broj3));
